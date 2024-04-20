06377163c6b9a6120132ac94d534e57260a3a868
deibidu <davidlopezfaustino@gmail.com>
2024-04-20 18:23:06 +0200

    correccion de algunas variables de sass que teniamos mal escritas, borrar unas apis que instalamos para nada, importar fuente Jonts de google fonts y añadir un checkbox y una funcion que al clickarlo tacha el item añadido a la lista

--- src/pages/lista-de-compras.tsx
+++ src/pages/lista-de-compras.tsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import "../sass/colors.scss";
 
 export function ListaCompra() {
-  const [items, setItems] = useState<{ id: string; value: string }[]>([]);
+  const [items, setItems] = useState<
+    { id: string; value: string; checked: boolean }[]
+  >([]);
   const [inputValue, setInputValue] = useState("");
 
   const addItem = () => {
     if (inputValue.trim()) {
-      setItems([...items, { id: Date.now().toString(), value: inputValue }]);
-      setInputValue("");
+      setItems([
+        ...items,
+        { id: Date.now().toString(), value: inputValue, checked: false },
+      ]);
+      setInputValue(" ");
     }
   };
 
@@ -16,23 +21,45 @@ export function ListaCompra() {
     setItems(items.filter((item) => item.id !== id));
   };
 
+  const handleOnChange = (id: string) => {
+    setItems(
+      items.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
   return (
     <>
       <div>
         <ul>
           {items.map((item) => (
-            <li key={item.id}>
+            <li
+              key={item.id}
+              style={{
+                textDecorationLine: item.checked ? "line-through" : "none",
+              }}
+            >
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleOnChange(item.id)}
+              />
+
               {item.value}
               <button onClick={() => handleRemove(item.id)}>-</button>
             </li>
           ))}
         </ul>
+
         <input
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button onClick={addItem}>+</button>
+        <button type="submit" onClick={addItem}>
+          +
+        </button>
       </div>
     </>
   );
