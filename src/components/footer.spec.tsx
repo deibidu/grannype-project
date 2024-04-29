d85d696b81a1adc519da89b08937259a2850b4f8
deibidu <davidlopezfaustino@gmail.com>
2024-04-29 23:41:38 +0200

    testing

---
+++ src/components/footer.spec.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { Footer } from "./footer";
+
+/*describe("Button", () => {
+  it("Should render a button with the text indicated", () => {
+    const text = "hola";
+
+    render(<Button>text</Button>);
+
+    const button = screen.getByText("hola");
+    expect(button).toBeInTheDocument();
+  });
+});
+*/
