7c7ea6490b7b58b46ca55ead9a184906d11913a4
Laura Gutiérrez <149421811+LauraGutierrezGG@users.noreply.github.com>
2024-05-04 21:15:16 +0200

    - continue with filters :SS

--- src/pages/recetario.tsx
+++ src/pages/recetario.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const recipes = [
-    id: 1, name: 'Tortilla de patata',
-]
-
-function recipeBook() {
+function RecipeBook() {
     const [search, setSearch] = useState('');
+    const [suggestions, setSuggestions] = useState(['Aceite', 'Cebolla', 'Patata', 'Arroz', 'Huevo']);
+
+    function handleSearch(event) {
+        setSearch(event.target.value);
+    }
+
+    /*Cómo indicar que hay que buscar las palabras en las recetas creadas pero no crear una lista con los nombres de las recetas, sino una función que busque entre todas las recetas */
 }
 
-const filteredRecipes = recipes.filter(recipe => recipe.name.toLowerCase().includes(search.toLowerCase()));
+export default RecipeBook
