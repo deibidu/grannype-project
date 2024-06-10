b485b0f43090f79cfb18c158c970a4f7a24ea381
LauraGutierrezGG <lauragutierrezgallud@gmail.com>
2024-06-10 19:29:27 +0200

    last recipes view DONE

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,37 @@
 import './Home.scss';
+import photoRecipeExample1 from '../assets/images/examplePhotoRecipe1.jpg';
+import photoRecipeExample2 from '../assets/images/examplePhotoRecipe2.jpg';
+import photoRecipeExample3 from '../assets/images/examplePhotoRecipe3.jpg';
+import arrowIconOrange from '../assets/images/arrowIconOrange.svg';
 
 export const Home = () => {
   return (
     <>
-      <h1 className={'font-title-sections'}>Your recipes are waiting for you</h1>;
+      <h1 className={'font-title-sections'}>Your recipes are waiting for you</h1>
       <div className="widgets-Home">
-        <div className="Home_widgetCard">
+        <div className="Home_widgetCard--no-border">
           <div className="Home_lastRecipes__container">
-            <p className="Home_widgetContainer">There's no recipes</p>
+            <div className="recipe-item">
+              <img src={photoRecipeExample1} alt="Recipe" className="recipe-image" />
+              <div className="recipe-content">
+                <span className="recipe-title">Ensalada César con aguacate</span>
+                <img src={arrowIconOrange} alt="Go to recipe" className="recipe-arrow" />
+              </div>
+            </div>
+            <div className="recipe-item">
+              <img src={photoRecipeExample2} alt="Recipe" className="recipe-image" />
+              <div className="recipe-content">
+                <span className="recipe-title">Tortitas con nata y fresas</span>
+                <img src={arrowIconOrange} alt="Go to recipe" className="recipe-arrow" />
+              </div>
+            </div>
+            <div className="recipe-item">
+              <img src={photoRecipeExample3} alt="Recipe" className="recipe-image" />
+              <div className="recipe-content">
+                <span className="recipe-title">Especias caseras</span>
+                <img src={arrowIconOrange} alt="Go to recipe" className="recipe-arrow" />
+              </div>
+            </div>
           </div>
         </div>
 
@@ -26,7 +50,6 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      ;
     </>
   );
 };
